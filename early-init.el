c9d9ce1f208dbd5c141cf8b4a4ac929f5cf0301d
George Kettleborough <george.kettleborough@owlstone.co.uk>
2023-09-05 12:50:36 -0600

    Suppress reporting of async compile warnings and errors
    Signed-off-by: Ashton Wiersdorf <mail@wiersdorf.dev>

--- early-init.el
+++ early-init.el
@@ -19,6 +19,7 @@
 (setq gc-cons-threshold 10000000)
 (setq byte-compile-warnings '(not obsolete))
 (setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
 
 ;; Silence stupid startup message
 (setq inhibit-startup-echo-area-message (user-login-name))
