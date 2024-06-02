fb96831f54e7337f92e615e812797823b341b79a
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-06-01 22:56:05 -0600

    Use user-emacs-directory for backups folder
    Closes #3

--- init.el
+++ init.el
@@ -79,7 +79,7 @@
 (defun bedrock--backup-file-name (fpath)
   "Return a new file path of a given file path.
 If the new path's directories does not exist, create them."
-  (let* ((backupRootDir "~/.emacs.d/emacs-backup/")
+  (let* ((backupRootDir (concat user-emacs-directory "emacs-backup/"))
          (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path
          (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
     (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
